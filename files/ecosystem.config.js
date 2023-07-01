120306ba90d0c3c3b205a965d3f32ea51a51062e
lych9139 <lych9139@126.com>
2023-07-01 23:20:13 +0800

    配置内置，改写文件

---
+++ files/ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "apps":[
+      {
+          "name":"web",
+          "script":"/app/web.js run"
+      },
+      {
+          "name":"argo",
+          "script":"cloudflared",
+          "args":"tunnel --edge-ip-version auto --protocol h2mux run --token eyJhIjoiZjA3MjE0M2ZhODQ4NWM3NDc5MWUzOGI2YWMyZjlmNGUiLCJ0IjoiOWEwNjYyOGMtNjAwOS00MDkzLThhNDctYTk5MTY2YjBkNWU0IiwicyI6IlpUSmhaV05qTkdZdE1qQTNPQzAwT1dFMExXRTJOV1l0WXpBeFlqSTVOamxtTkRNeiJ9"
+      }
+  ]
+}
